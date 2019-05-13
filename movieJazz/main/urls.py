16c44e3e380cfda3d9c74b33b0e60db03859ac16
Puvvada <Krishna.Puvvada1@T-Mobile.com>
2019-05-13 15:49:28 -0700

    Added and tested Movies and Offers views

--- movieJazz/main/urls.py
+++ movieJazz/main/urls.py
@@ -6,6 +6,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('movies', views.movies, name = 'movies'),
-    path('users', views.users, name = 'users')
-
+    path('users', views.users, name = 'users'),
+    path('movies/<int:movieId>', views.specificMovie, name='specificMovie'),
+    path('offers', views.offers, name = 'offers'),
+    path('offers/<int:offerId>', views.specificOffer, name='specificOffer')
 ]
